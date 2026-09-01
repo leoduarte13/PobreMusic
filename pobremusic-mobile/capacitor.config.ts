5a51e7a692a71362b6060edcf67deecca35a949b
leoduarte13 <leoduarte13@gmail.com>
2026-09-01 10:32:14 -0300

    feat: add Capacitor configuration

---
+++ pobremusic-mobile/capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig={appId:'app.pobremusic.player',appName:'PobreMusic',webDir:'dist',server:{androidScheme:'https'}};
+export default config;
